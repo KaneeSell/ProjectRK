fb7215196528a8e3e0087e7e0715da98ee29cc04
Daniel Henrique Sell <kanee.sell@gmail.com>
2024-05-22 00:53:53 -0300

    url commit

--- agenda/script.js
+++ agenda/script.js
@@ -35,5 +35,5 @@ salvarBtn.addEventListener('click', () => {
 document.getElementById('voltar_menu').addEventListener('click', function(event) {
     event.preventDefault();
     alert('Voltando ao Menu!');
-    window.location.href = "ProjectRK/menu/menu.html";
+    window.location.href = "/ProjectRK/menu/menu.html";
 });
--- menu/script.js
+++ menu/script.js
@@ -1,20 +1,20 @@
 document.getElementById('ir_agenda').addEventListener('click', function(event) {
     event.preventDefault();
     alert('Navegando para Agenda!');
-    window.location.href = "ProjectRK/agenda/agenda.html";
+    window.location.href = "/ProjectRK/agenda/agenda.html";
 });
 document.getElementById('ir_pagina2').addEventListener('click', function(event) {
     event.preventDefault();
     alert('Navegando para pagina2!');
-    window.location.href = "ProjectRK/pagina2/pagina2.html";
+    window.location.href = "/ProjectRK/pagina2/pagina2.html";
 });
 document.getElementById('ir_pagina3').addEventListener('click', function(event) {
     event.preventDefault();
     alert('Navegando para pagina3!');
-    window.location.href = "ProjectRK/pagina3/pagina3.html";
+    window.location.href = "/ProjectRK/pagina3/pagina3.html";
 });
 document.getElementById('ir_pagina4').addEventListener('click', function(event) {
     event.preventDefault();
     alert('Navegando para pagina4!');
-    window.location.href = "ProjectRK/pagina4/pagina4.html";
+    window.location.href = "/ProjectRK/pagina4/pagina4.html";
 });
