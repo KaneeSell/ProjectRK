735d4da7f3f052bf56157c4d18dd27cb056fed3a
Daniel Henrique Sell <kanee.sell@gmail.com>
2024-05-21 22:16:31 -0300

    button menu commit

--- menu/script.js
+++ menu/script.js
@@ -1,4 +1,4 @@
-document.getElementById('Agenda-form').addEventListener('Agenda', function(event) {
+document.getElementById('ir_agenda').addEventListener('click', function(event) {
     event.preventDefault();
     alert('Navegando para Agenda!');
     window.location.href = "pagina2/pagina2.html";
